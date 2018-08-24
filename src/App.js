ca25d757cf504a78ba4d50ff859b046e9ada63f4
Ish Tahir <ishtahir@icloud.com>
2018-08-23 19:18:28 -0500

    Google maps has appeared on the screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import GoogleMapReact from 'google-map-react';
 import './App.css';
 
 class App extends Component {
+    state = {
+        center: {lat: 40.73, lng: -73.93},
+        zoom: 12
+    };
+
     render() {
         return (
-            <div className="App">
-                test
+            <div className="App" style={{ height: '100vh', width: '100%'}}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: 'AIzaSyDYDXMsbIg9zQq37WQwH4Fe0azIhSg7kcY'}}
+                    defaultCenter={this.props.center}
+                    center={this.state.center}
+                    defaultZoom={this.state.zoom}
+                ></GoogleMapReact>
             </div>
         );
     }
