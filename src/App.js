d087d75a5522c338f119e60850ac0e2d2d639337
Ish Tahir <ishtahir@icloud.com>
2018-08-25 15:37:20 -0500

    Updated CSS for App.js, got the sidebar to appear, removed code for google-map-react module

--- src/App.js
+++ src/App.js
@@ -1,66 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import GoogleMapReact from 'google-map-react';
+import Sidebar from './components/Sidebar.jsx';
 import './App.css';
 
-class GoogleMap extends Component {
-    static defaultProps = {
-        center: {lat: 30.4548, lng: -97.6223},
-        zoom: 13
+class App extends Component {
+    state = {
+        mapIsReady: false,
     };
 
+    componentDidMount() {
+        const apiKey = 'AIzaSyBuOtZ4Xw9Wo4EK96HJ5Xmrs4Rz9sv5P1c';
+        const script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+        script.async = true;
+        script.defer = true;
+        script.addEventListener('load', () => {
+            // this.setState({ mapIsReady: true });
+        });
+
+        document.body.appendChild(script);
+    }
+
+    componentDidUpdate() {
+        if (this.state.mapIsReady) {
+            // Display the map
+            this.map = new window.google.maps.Map(document.getElementById('map'), {
+                center: {lat: 30.4548, lng: -97.6223},
+                zoom: 13,
+                mapTypeId: 'roadmap',
+            });
+            // You also can add markers on the map below
+        }
+    }
+
     render() {
+        const style = {
+            height: '90vh',
+            width: '70%',
+            display: 'flex',
+            float: 'right'
+        }
         return (
-            <div className="App" style={{ height: '100vh', width: '100%'}}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{key: 'AIzaSyBuOtZ4Xw9Wo4EK96HJ5Xmrs4Rz9sv5P1c'}}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                ></GoogleMapReact>
+            <div className="app-container">
+                <header className="header">
+                    <h1 className="title">Map of Pflugerville, TX</h1>
+                </header>
+                <Sidebar />
             </div>
         );
     }
 }
 
-export default GoogleMap;
-
-// import React, { Component } from 'react';
-//
-// class GoogleMap extends Component {
-//     state = {
-//         mapIsReady: false,
-//     };
-//
-//     componentDidMount() {
-//         const apiKey = 'AIzaSyBuOtZ4Xw9Wo4EK96HJ5Xmrs4Rz9sv5P1c';
-//         const script = document.createElement('script');
-//         script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
-//         script.async = true;
-//         script.defer = true;
-//         script.addEventListener('load', () => {
-//             this.setState({ mapIsReady: true });
-//         });
-//
-//         document.body.appendChild(script);
-//     }
-//
-//     componentDidUpdate() {
-//         if (this.state.mapIsReady) {
-//             // Display the map
-//             this.map = new window.google.maps.Map(document.getElementById('map'), {
-//                 center: {lat: 30.4548, lng: -97.6223},
-//                 zoom: 13,
-//                 mapTypeId: 'roadmap',
-//             });
-//             // You also can add markers on the map below
-//         }
-//     }
-//
-//     render() {
-//         return (
-//             <div id="map" style={{height: '100vh', width: '100%'}} />
-//         );
-//     }
-// }
-//
-// export default GoogleMap;
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+    render() {
+        const style = {
+            height: '100vh',
+            width: '30%',
+            backgroundColor: 'tomato',
+            float: 'left'
+        }
+        return (
+            <div className="sidebar" style={style}>
+                
+            </div>
+        )
+    }
+}
+
+export default Sidebar;
