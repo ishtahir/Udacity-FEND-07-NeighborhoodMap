76d5bd302ab3fd9a9e176c7a1862dbd92d98836a
Ish Tahir <ishtahir@icloud.com>
2018-08-26 15:49:11 -0500

    Added all locations to sidebar as <li> updated the CSS

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
             {name: 'Pflugerville High School',loc: {lat: 30.4448663, lng: -97.6337909}, address: '1301 W Pecan St, Pflugerville, TX 78660'},
             {name: 'Hendrickson High School',loc: {lat: 30.4656629, lng: -97.5878583}, address: '19201 Colorado Sand Dr, Pflugerville, TX 78660'},
             {name: 'Weiss High School',loc: {lat: 30.4291442, lng: -97.56574520000001}, address: '5201 Wolf Pack Dr, Pflugerville, TX 78660'},
-            {name: 'Pflugerville Academic Center of Excellence (PACE)',loc: {lat: 30.446406, lng: -97.63657909999999}, address: '1401-B W Pecan St, Pflugerville, TX 78660'}
+            {name: 'Pflugerville Academic Center of Excellence',loc: {lat: 30.446406, lng: -97.63657909999999}, address: '1401-B W Pecan St, Pflugerville, TX 78660'}
         ]
     }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 class Sidebar extends Component {
     render() {
+        const { locations } = this.props;
         return (
             <div className="sidebar">
                 <input type="text" className="search" placeholder="Filter items" />
+                <ul>
+                    {locations.map((location, index) => {
+                        return <li key={index} className="sidebar-item">{location.name}</li>
+                    })}
+                </ul>
             </div>
         )
     }
