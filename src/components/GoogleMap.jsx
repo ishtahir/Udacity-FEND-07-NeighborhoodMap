d9fc9388bdf2cbaae6d2b5a511d55d2eeb2e2256
Ish Tahir <ishtahir@icloud.com>
2018-08-25 20:54:36 -0500

    Made markers show up on the map for my data

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -3,6 +3,13 @@ import React, { Component } from 'react';
 class GoogleMap extends Component {
     state = {
         mapIsReady: false,
+        locations: [
+            {name: 'Connally High School', loc: {lat: 30.4192234, lng: -97.6762375}},
+            {name: 'Pflugerville High School',loc: {lat: 30.4448663, lng: -97.6337909}},
+            {name: 'Hendrickson High School',loc: {lat: 30.4656629, lng: -97.5878583}},
+            {name: 'Weiss High School',loc: {lat: 30.4291442, lng: -97.56574520000001}},
+            {name: 'Pflugerville Academic Center of Excellence',loc: {lat: 30.446406, lng: -97.63657909999999}}
+        ]
     };
 
     componentDidMount() {
@@ -26,8 +33,18 @@ class GoogleMap extends Component {
                 zoom: 13,
                 mapTypeId: 'roadmap',
             });
-            // You also can add markers on the map below
+            this.state.locations.map(location => {
+                this.generateMarkers(location.loc, this.map, location.name)
+            });
         }
+    }
+
+    generateMarkers(pos, map, title) {
+        const marker = new window.google.maps.Marker({
+            position: pos,
+            map: map,
+            title: title
+        });
     }
 
     render() {
