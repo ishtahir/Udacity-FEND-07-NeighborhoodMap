5d620d783ea5c1235a94a511c5678e8fe056c1f1
Ish Tahir <ishtahir@icloud.com>
2018-08-26 15:32:30 -0500

    Moved locations prop to the parent component, added search input, edited CSS

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ class Sidebar extends Component {
     render() {
         return (
             <div className="sidebar">
-
+                <input type="text" className="search" placeholder="Filter items" />
             </div>
         )
     }
